47cfd0fe5beb29be1e373320f20bcd7f19843f97
wfgp12 <w.gutierrez1206@gmail.com>
2025-01-11 21:27:07 -0500

    feat: add user registration form
    Implemented a form that allows users to register in the application. Includes:
    - Validation for fields such as name, email, and password.
    - Integration with Firebase to register new users.
    - Error messages and visual feedback to enhance user experience.

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,34 +1,74 @@
+import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 
 import { AuthLayout } from '../layout'
+import { useForm } from '../../hooks/useForm';
+
+const formData = {
+  email: '',
+  password: '',
+  displayName: ''
+}
+
+const formValidations = {
+  email: [(value) => value.includes('@'), 'Ingrese un email valido'],
+  password: [(value) => value.length >=6, 'La contraseña debe tener al 6 caracteres'],
+  displayName: [(value) => value.length >=1, 'El nombre es requerido'],
+}
 
 export const RegisterPage = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const { 
+    formState, displayName, email, password, onInputChange,
+    isFormValid, displayNameValid, emailValid, passwordValid, 
+  } = useForm(formData, formValidations);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true)
+  }
+
   return (
     <AuthLayout title="Crear cuenta">
-      <form action="">
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 3 }}>
             <TextField
               label="Nombre completo"
+              name='displayName'
+              value={displayName}
+              onChange={onInputChange}
               type="text"
               placeholder="tu nombre"
+              error={!!displayNameValid && formSubmitted}
+              helperText={displayNameValid}
               fullWidth
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 3 }}>
             <TextField
               label="Correo"
+              name='email'
+              value={email}
+              onChange={onInputChange}
               type="email"
               placeholder="email@example.com"
+              error={!!emailValid && formSubmitted}
+              helperText={emailValid}
               fullWidth
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 3 }}>
             <TextField
               label="Contraseña"
+              name='password'
+              value={password}
+              onChange={onInputChange}
               type="password"
               placeholder="contraseña"
+              error={!!passwordValid && formSubmitted}
+              helperText={passwordValid}
               fullWidth
             />
           </Grid>
@@ -37,14 +77,20 @@ export const RegisterPage = () => {
             sx={{ mb: 2, mt: 1 }}
           >
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>Crear cuenta</Button>
+              <Button
+                type='submit'
+                variant="contained"
+                fullWidth
+              >
+                Crear cuenta
+              </Button>
             </Grid>
           </Grid>
           <Grid container
             direction="row"
             justifyContent="end"
           >
-            <Typography sx={{mr:1}}>¿Ya tienes cuenta? </Typography>
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta? </Typography>
             <Link component={RouterLink} color="inherit" to="/auth/login">
               inicia sesión
             </Link>
